2d62ec0f194b34f50f6b8e3ffed6b4930a672d5f
antoniocostantin <costantini062@gmail.com>
2024-08-30 16:52:17 +0200

    devo sistema

---
+++ src/component/SingleCard.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { Card, Col } from "react-bootstrap"
+
+
+const SingleCard = ({w}) => { 
+    const [icon, setIcon] = useState("");
+
+
+    useEffect(()=> {
+        setIcon(
+            "http://openweathermap.org/img/w/" + w.weather[0].icon + ".png"
+          );
+    }, [])
+    
+    useEffect(()=> {
+        setIcon(
+            "http://openweathermap.org/img/w/" + w.weather[0].icon + ".png"
+          );
+    }, [w])
+    return (
+        <Col>
+        <Card bg="dark" key="Dark" text="white" className="mb-2">
+          <Card.Header className="text-capitalize">
+            <img src={icon} alt="icon" className="iconw" /> {w.weather[0].description}
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <i className="bi bi-thermometer-low"></i>
+              {w.main.temp_min}°C -- <i className="bi bi-thermometer-high"></i>
+              {w.main.temp_min}°C <br />
+              <i className="bi bi-water"></i> {w.main.humidity} % <br />
+              <i className="bi bi-wind"></i> {w.wind.speed} <small>m/s</small> -- {w.wind.deg} <small>deg</small><hr/>
+              <small >{w.dt_txt.slice(0, 10)} </small>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+    )
+}
+
+export default SingleCard
